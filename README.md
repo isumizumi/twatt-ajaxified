87efafa349e4a20eaed87364dd0a5b56b75b89ab
isumizumi <isumi.karina72@gmail.com>
2017-03-06 18:35:34 +0700

    Finish Post Tweet

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ index.html in folder client
 #### Adding npm:
 > npm install --save cors
 
+> npm install --save twitter-enhanced <br>
+//for Tweet API POST request
+
 P.s:
-Adding require & app.use for cors in app.js
+Adding require & app.use for cors in app.js, and twitter-enhanced in routes/index.js
 
 #### Don't forget to:
 npm start (folder server/twatt-recent)
--- client/tweet.js
+++ client/tweet.js
@@ -5,13 +5,28 @@ function getTweet(){
     success: function(data) {
       data = JSON.parse(data)
       // console.log(data);
-      // $('#dataTweet').prepend('')
       var listTweet = ''
       for (var i = 0; i < data.length; i++) {
         listTweet += `<tr><td>${data[i].user.screen_name}</td><td>${data[i].text}</td><td><img src='${data[i].user.profile_image_url}'></td><td>${data[i].created_at}</td></tr>`
       }
       $('#dataTweet tbody').html('')
       $('#dataTweet tbody').append(listTweet)
+    },
+    fail: function(err) {
+      console.log(err);
+    }
+  })
+}
+
+function postTweet(){
+  $.ajax({
+    url: 'http://localhost:3000/status',
+    method: 'POST',
+    data: {
+      status: $("#status").val()
+    },
+    success: function(data) {
+      console.log(data);
     },
     fail: function(err) {
       console.log(err);
